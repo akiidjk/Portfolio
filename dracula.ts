62d30a65c1d4800c5b52f28338e6f895e665e59b
Retr0 <dragonfaiver@gmail.com>
2024-06-13 14:31:36 +0200

    Added Navbar.svelte

--- dracula.ts
+++ dracula.ts
@@ -1,11 +1,11 @@
-import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+import type {CustomThemeConfig} from '@skeletonlabs/tw-plugin';
 
 export const dracula: CustomThemeConfig = {
     name: 'dracula',
     properties: {
         // =~= Theme Properties =~=
         "--theme-font-family-base": `ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace`,
-        "--theme-font-family-heading": `system-ui`,
+        "--theme-font-family-heading": `Inter, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
         "--theme-font-color-base": "0 0 0",
         "--theme-font-color-dark": "255 255 255",
         "--theme-rounded-base": "12px",
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,57 @@
+<script>
+    import {LightSwitch,} from "@skeletonlabs/skeleton";
+    import {onMount} from 'svelte';
+    import {Menu} from '@svelteuidev/core';
+    import {BookmarkFilled, HamburgerMenu, Home, LockClosed, Person, Share1} from 'radix-icons-svelte';
+
+    let width = 0;
+    
+    const updateDimensions = () => {
+        width = window.innerWidth;
+    };
+
+    onMount(() => {
+        updateDimensions();
+        window.addEventListener('resize', updateDimensions);
+
+        return () => {
+            window.removeEventListener('resize', updateDimensions);
+        };
+    });
+
+</script>
+
+<nav class="fixed w-full z-20 shadow-lg flex">
+    <div class="bg-transparent backdrop-blur-sm w-full h-20 flex justify-between items-center">
+        <div class="mx-10 flex items-center">
+            <img src="spidy.jpg" alt="Logo" class="w-14 h-14 rounded-full object-cover object-center mr-10"/>
+            <h1 class="text-4xl font-medium">Akiidjk</h1>
+        </div>
+        <div class="flex mr-10">
+            {#if width < 760}
+                <Menu transitionOptions={{ y: 200 }}>
+                    <button slot="control">
+                        <HamburgerMenu size={24}/>
+                    </button>
+                    <Menu.Label>Menu</Menu.Label>
+                    <Menu.Item icon={Home}><a class="mx-5" href="/">Home</a></Menu.Item>
+                    <Menu.Item icon={Person}><a class="mx-5" href="/">About</a></Menu.Item>
+                    <Menu.Item icon={BookmarkFilled}><a class="mx-5" href="/">Projects</a></Menu.Item>
+                    <Menu.Item icon={Share1}><a class="mx-5" href="/">Contacts</a></Menu.Item>
+                    <Menu.Item icon={LockClosed}><a class="mx-5" href="/">Admin</a></Menu.Item>
+                    <Menu.Item>
+                        <LightSwitch/>
+                    </Menu.Item>
+                </Menu>
+            {:else}
+                <a class="mx-5" href="/">Home</a>
+                <a class="mx-5" href="/">About</a>
+                <a class="mx-5" href="/">Projects</a>
+                <a class="mx-5" href="/">Contacts</a>
+                <a class="mx-5" href="/">Admin</a>
+                <LightSwitch/>
+            {/if}
+        </div>
+    </div>
+</nav>
+<div class="h-20"></div>
