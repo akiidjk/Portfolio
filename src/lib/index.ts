e5d5afb4fb0efca12ec0d3ff68ff54c412dce01b
Akiidjk <dragonfaiver@gmail.com>
2024-07-28 11:12:49 +0200

    Added more scene and models

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,8 +3,14 @@ import LoaderCore from './components/ui/LoaderCore.svelte';
 import MultiStepLoader from './components/ui/MultiStepLoader.svelte';
 import Porsche911 from './models/Porsche911.svelte';
 import Porsche2022 from './models/Porsche2022.svelte';
+import SushiSet from './models/SushiSet.svelte';
 import JapaneseRestaurant from './models/JapaneseRestaurant.svelte';
+import JapaneseTeaShop from './models/JapaneseTeaShop.svelte';
+
 import SceneHome from '$lib/scenes/SceneHome.svelte';
+import SceneSushiSet from '$lib/scenes/SceneSushiSet.svelte';
+import SceneTeaShop from '$lib/scenes/SceneTeaShop.svelte';
+
 import { cn } from './utils/cn';
 
 export {
@@ -14,6 +20,10 @@ export {
 	Porsche911,
 	Porsche2022,
 	JapaneseRestaurant,
+	JapaneseTeaShop,
+	SushiSet,
 	SceneHome,
+	SceneSushiSet,
+	SceneTeaShop,
 	cn
 };
---
+++ src/lib/scenes/SceneTeaShop.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { T } from '@threlte/core';
+	import { OrbitControls } from '@threlte/extras';
+	import { JapaneseTeaShop } from '$lib';
+</script>
+
+<!--npx @threlte/gltf@latest .\japanese_restaurant.glb --transform-->
+
+<!-- Configurazione della telecamera prospettica con controlli orbitali -->
+<T.PerspectiveCamera makeDefault position={[1, 1, 0]}>
+	<OrbitControls
+		enablePan={false}
+		enableDamping
+		maxDistance={10}
+		minDistance={10}
+		autoRotateSpeed={3.0}
+	/>
+</T.PerspectiveCamera>
+
+<!-- Luce ambientale per illuminazione generale -->
+<T.AmbientLight intensity={0.5} />
+<T.DirectionalLight position={[15, 10, 10]} intensity={3} castShadow />
+<T.DirectionalLight />
+<T.PointLight position={[5, 5, 5]} intensity={1} castShadow />
+<T.SpotLight position={[5, 5, 5]} angle={0.3} intensity={100} />
+
+<JapaneseTeaShop scale={1.5} position={[0, -2, 0]} rotation={[0, 1.2, 0]} />
