2e0bd9bdd408b2436f47e436778a4dc6cc7a3853
Akiidjk <dragonfaiver@gmail.com>
2024-08-02 10:01:40 +0200

    Remove unused exports and models from the library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,14 +3,8 @@ import Blob from './components/Blob.svelte';
 
 import LoaderCore from './components/ui/LoaderCore.svelte';
 import MultiStepLoader from './components/ui/MultiStepLoader.svelte';
-import Porsche911 from './models/Porsche911.svelte';
-import Porsche2022 from './models/Porsche2022.svelte';
-import SushiSet from './models/SushiSet.svelte';
-import JapaneseRestaurant from './models/JapaneseRestaurant.svelte';
 import JapaneseTeaShop from './models/JapaneseTeaShop.svelte';
 
-import SceneHome from '$lib/scenes/SceneHome.svelte';
-import SceneSushiSet from '$lib/scenes/SceneSushiSet.svelte';
 import SceneTeaShop from '$lib/scenes/SceneTeaShop.svelte';
 
 import { cn } from './utils/cn';
@@ -20,13 +14,7 @@ export {
 	Blob,
 	LoaderCore,
 	MultiStepLoader,
-	Porsche911,
-	Porsche2022,
-	JapaneseRestaurant,
 	JapaneseTeaShop,
-	SushiSet,
-	SceneHome,
-	SceneSushiSet,
 	SceneTeaShop,
 	cn
 };
