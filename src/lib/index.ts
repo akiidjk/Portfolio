f265d71bb381ea35af533d29068a08fae8d5a206
Akiidjk <dragonfaiver@gmail.com>
2024-08-16 18:04:24 +0200

    chore: Add StarsBackground and ShootingStars components to the library

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,8 +2,12 @@ import Navbar from './components/Navbar.svelte';
 import Blob from './components/Blob.svelte';
 import CheckIcon from './components/CheckIcon.svelte';
 
+
 import LoaderCore from './components/ui/LoaderCore.svelte';
 import MultiStepLoader from './components/ui/MultiStepLoader.svelte';
+import StarsBackground from './components/ui/StarsBackground.svelte';
+import ShootingStars from './components/ui/ShootingStars.svelte';
+
 import JapaneseTeaShop from './models/JapaneseTeaShop.svelte';
 
 import SceneTeaShop from '$lib/scenes/SceneTeaShop.svelte';
@@ -15,7 +19,9 @@ export {
 	Blob,
 	CheckIcon,
 	LoaderCore,
+	StarsBackground,
 	MultiStepLoader,
+	ShootingStars,
 	JapaneseTeaShop,
 	SceneTeaShop,
 	cn
